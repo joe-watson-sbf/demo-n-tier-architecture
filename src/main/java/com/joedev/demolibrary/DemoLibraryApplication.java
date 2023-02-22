71510691d3c2dcb4a87b1e836ff86d26e60435e2
Joe Watson SBF <joewatson40@hotmail.com>
2023-02-21 22:56:42 -0500

    first commit

---
+++ src/main/java/com/joedev/demolibrary/DemoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.joedev.demolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joedev/demolibrary/DemoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joedev.demolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
